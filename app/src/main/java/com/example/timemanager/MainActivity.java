6c51d082ed135ae2fdc4e3ac9c560148b89c3263
ouyang1229 <2455691605@qq.com>
2019-03-31 14:32:31 +0800

    show

--- app/src/main/java/com/example/timemanager/MainActivity.java
+++ app/src/main/java/com/example/timemanager/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.timemanager;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +15,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewStub;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    String[] p_num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +33,15 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                showCustomizeDialog();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
 
@@ -41,6 +54,65 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+//dialog
+private void showCustomizeDialog() {
+    /* @setView 装入自定义View ==> R.layout.dialog_customize
+     * 由于dialog_customize.xml只放置了一个EditView，因此和图8一样
+     * dialog_customize.xml可自定义更复杂的View
+     */
+
+
+    AlertDialog.Builder customizeDialog =
+            new AlertDialog.Builder(MainActivity.this);
+
+    final View dialogView = LayoutInflater.from(MainActivity.this)
+            .inflate(R.layout.add_task_dialog,null);
+
+    customizeDialog.setTitle("Add a new task");
+    customizeDialog.setView(dialogView);
+
+
+    p_num = getResources().getStringArray(R.array.pomodoronum);
+
+    customizeDialog.setPositiveButton("OK",
+            new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // 获取EditView中的输入内容
+                    EditText edit_text = (EditText) dialogView.findViewById(R.id.et_new_task_name);
+                    Toast.makeText(MainActivity.this,
+                            edit_text.getText().toString(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+    customizeDialog.setNegativeButton("CANCEL",
+            new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //...To-do
+                }
+            });
+
+    customizeDialog.show();
+//    Spinner pSpinner = (Spinner) findViewById(R.id.pomodoro_num);
+//    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, p_num);
+//    pSpinner.setAdapter(adapter);
+//    pSpinner.setOnItemSelectedListener(
+//            new AdapterView.OnItemSelectedListener() {
+//                @Override
+//                public void onItemSelected(AdapterView<?> arg0,
+//                                           View arg1, int arg2, long arg3) {
+//                    int index = arg0.getSelectedItemPosition();
+//                    Toast.makeText(getBaseContext(),
+//                            "You have selected item : " + p_num[index], Toast.LENGTH_LONG).show();
+//                }
+//                @Override
+//                public void onNothingSelected(AdapterView<?> arg0) {
+//                }
+//            });
+}
+
+
 
     @Override
     public void onBackPressed() {
